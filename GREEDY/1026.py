de6796adad939e937a5eba3afaf780a5b194d140
yeeun66 <22200450@handong.ac.kr>
2023-08-20 08:08:47 +0900

    해결

--- GREEDY/1026.py
+++ GREEDY/1026.py
@@ -1,2 +1,15 @@
 # 보물
+# S = A[0] × B[0] + ... + A[N-1] × B[N-1]
+# A의 수를 재배열. B에 있는 수는 재배열X => S의 최솟값 구하기
 n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+sorted_a = sorted(a)
+sorted_b = sorted(b)
+sorted_a.reverse()
+
+s = 0
+for i in range(n) :
+    s += sorted_a[i] * sorted_b[i]
+print(s)
