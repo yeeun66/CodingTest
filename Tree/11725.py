08d62260167e1c0585c1ec7a597e7ac0799f8858
yeeun66 <22200450@handong.ac.kr>
2025-07-26 23:32:13 +0900

    7/26(Sat) Tree

--- Tree/11725.py
+++ Tree/11725.py
@@ -1,7 +1,32 @@
 # 트리의 부모 찾기
-# 루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
 
-# 첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
-# 첫째 줄부터 N-1개의 줄에 각 노드의 부모 노드 번호를 2번 노드부터 순서대로 출력한다.
+'''
+로직
+1. 입력으로 양방향 그래프 구축
+2. 1번 노드부터 bfs 탐색하며 자식관계 딕셔너리 제작
+3. 제작된 자식관계 딕셔너리 2번 노드부터 출력 
+'''
 
-# DFS인 것 같으니 잠시 보류
+from collections import deque 
+
+N = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(N-1) :
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+parent = {}
+
+visited = [0] * (N+1)
+que = deque([1])
+visited[1] = 1
+while que : 
+    cur = que.popleft()
+    for g in graph[cur] :
+        if not visited[g] : 
+            visited[g] = 1
+            que.append(g)
+            parent[g] = cur
+
+for i in range(2, N+1) : print(parent[i])
